cac767bcb099bf5677b0240e2abedbfaa57cab4c
kjb244 <kevin.bacino@gmail.com>
2018-09-14 18:48:21 -0400

    using sendgrid

--- routes/index.js
+++ routes/index.js
@@ -36,16 +36,7 @@ router.get('/getContent', function(req, res){
 });
 
 router.get('/testEmail', function(req,res){
-    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'kbacino244@yahoo.com',
-        from: 'test@example.com',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg);
+
     return res.json({});
 });
 
