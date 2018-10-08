39386191217fa04c1f779eeb68e46496fb5263ba
kjb244 <kevin.bacino@gmail.com>
2018-10-08 09:54:16 -0400

    compressed images and added script to do so

---
+++ utils/compressImages.js
@@ -0,0 +1,14 @@
+const compress_images = require('compress-images');
+let INPUT_path_to_your_images, OUTPUT_path;
+
+INPUT_path_to_your_images = 'temp/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+OUTPUT_path = 'build/';
+
+
+compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: true}, false,
+    {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+    {png: {engine: 'pngquant', command: ['--quality=20-50']}},
+    {svg: {engine: 'svgo', command: '--multipass'}},
+    {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(){
+
+});
